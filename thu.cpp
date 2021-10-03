d556fe17a0b3768983a3b114af6dcd330a20b6bb
19522379hoThuTrang <19522379@gm.uit.edu.com.vn>
2021-10-03 20:10:51 +0700

    first commit

---
+++ thu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>;
+using namespace std;6
+int main()
+{
+    cout<<"Hello ";
+    cout<<"nhập a, b";
+    int a,b;
+    cin>>a;
+    cin>>b;
+    cout <<a+b;
+    return 0;
+}
+# cuaHangDaQui
